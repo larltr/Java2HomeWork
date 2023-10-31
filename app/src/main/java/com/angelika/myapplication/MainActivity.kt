bc40e88a2ab13a13156377cef3e838f7763d56e8
larltr <larltr17@gmail.com>
2023-10-31 23:33:01 +0600

    Initial commit

---
+++ app/src/main/java/com/angelika/myapplication/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.angelika.myapplication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.angelika.myapplication.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
